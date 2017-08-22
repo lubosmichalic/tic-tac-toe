b7c7cb84f05ccf8ac83c502278aed536c95ce71d
Lubos Michalic <lnmichalic@gmail.com>
2017-08-22 15:11:12 +0100

    Fill in an available space with a symbol

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -4,4 +4,8 @@ class GameBoard
   def initialize
     @tiles = [ '1','2','3','4','5','6','7','8','9' ]
   end
+
+  def fill_in(position, symbol)
+    tiles[position - 1] = symbol
+  end
 end
--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -4,4 +4,9 @@ describe GameBoard do
   it 'initializes with 9 tiles' do
     expect(subject.tiles).to eq ['1', '2', '3', '4', '5', '6', '7', '8', '9']
   end
+
+  it 'is able to fill a space with an x' do
+    subject.fill_in(6, 'x')
+    expect(subject.tiles).to eq ['1', '2', '3', '4', '5', 'x', '7', '8', '9']
+  end
 end
